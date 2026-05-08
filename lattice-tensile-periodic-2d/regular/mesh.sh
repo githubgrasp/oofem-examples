30ede92f8681846640c4375ef1cb7b1e27584e7f
githubgrasp <peter.grassl@glasgow.ac.uk>
2026-05-08 14:45:38 +0100

    update 2d lattice random case

--- lattice-tensile-periodic-2d/regular/mesh.sh
+++
@@ -1,4 +0,0 @@
-~/build/oofem/src/generator/generator.exec mesh.in
-mv nodes.dat mesh.nodes
-qvoronoi p Fv < mesh.nodes > mesh.voronoi
-~/build/oofem/src/converter/converter.exec control.in mesh.nodes mesh.voronoi
